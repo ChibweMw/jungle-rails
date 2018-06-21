b4023bfdf2e0e3cbbf9d93241620fba52355e72a
Chibwe Mweene <caleidosymmetry@gmail.com>
2018-06-21 16:41:22 +0000

    minor code styling changes

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,21 +1,21 @@
 class Admin::CategoriesController < ApplicationController
   def index
-      @categories = Category.order(id: :desc).all
-    end
+    @categories = Category.order(id: :desc).all
+  end
 
-    def new
-      @category = Category.new
-    end
+  def new
+    @category = Category.new
+  end
 
-    def create
-      @category = Category.new(category_params)
+  def create
+    @category = Category.new(category_params)
 
-      if @category.save
-        redirect_to [:admin, :categories], notice: 'Category created!'
-      else
-        render :new
-      end
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
     end
+  end
 
   private
 
