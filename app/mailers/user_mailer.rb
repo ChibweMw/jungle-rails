df7172d935722bbf8323c37fdf5a8b148b1bacc3
Chibwe Mweene <caleidosymmetry@gmail.com>
2018-06-22 04:58:36 +0000

    admin sections now restricted

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,11 +3,6 @@ class UserMailer < ApplicationMailer
 
   def order_receipt_email(order)
     @order = order
-    puts "==================================="
-    puts "WELCOME", @order
-    puts "==================================="
-
-    # @url = '/orders/:id'
     mail(to: @order.email, subject: @order.id)
   end
 end
