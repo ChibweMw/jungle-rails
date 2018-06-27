b4336bee839214636d36c70f7148b774a48e5b38
Chibwe Mweene <caleidosymmetry@gmail.com>
2018-06-27 03:35:40 +0000

    add-to-cart test passing

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "Add product to cart", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They add a product to the cart" do
+    visit root_path
+
+    first('article.product').find_link('Add').click
+
+    save_screenshot
+  end
+
+end
