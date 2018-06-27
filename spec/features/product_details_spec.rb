5f25f77b01bae03e52140cdded872505a74e1a56
Chibwe Mweene <caleidosymmetry@gmail.com>
2018-06-27 03:12:18 +0000

    product page access test passing

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "Navigate from homepage to individual product page", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see product detail page" do
+    visit root_path
+
+    first('article.product').find_link('Details »').click
+
+    sleep 5
+
+    save_screenshot
+  end
+end
